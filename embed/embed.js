3d072cf9b788376f9ec2d718af364c94d59d375f
Corey Moen <60983313+CoreyMoen@users.noreply.github.com>
2022-03-19 21:51:31 -0500

    Add files via upload

---
+++ embed/embed.js
@@ -0,0 +1,3 @@
+document.querySelector('.rango-notice_close').addEventListener('click', function() {
+        document.querySelector('.rango-notice_close').closest('#rango-notice').style.display = 'none';
+    });
